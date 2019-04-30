e6afd6f5ff1d6e30932e5b58a6f33aab47e709ce
veroena <veronica.enamorado@gmail.com>
2019-05-01 01:19:45 +0200

    Delete comments and console.logs

--- js/main.js
+++ js/main.js
@@ -14,11 +14,9 @@ function getRandomNumber(min, max) {
 
 const showPoke = () => {
   const pokeNumber = getRandomNumber(1, 807);
-  console.log(pokeNumber);
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}/`)
   .then (response => response.json())
   .then (data => {
-    console.log(data);
     const strengthNumber = getRandomNumber(0, 6);
     pokeName.innerHTML = `Hello, my name is ${data.name} ${strengthArr[strengthNumber]}`;
     img1.src = data.sprites.front_default;
@@ -27,38 +25,3 @@ const showPoke = () => {
 }
 
 btn.addEventListener('click', showPoke);
-
-
-// const alterEgo = () => {
-  
-//   const nameNumber = getRandomNumber(213, 1)
-//   fetch(`https://www.anapioficeandfire.com/api/characters?page=${nameNumber}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     const number = getRandomNumber(10, 0);
-//     const splitName = data[number].name.split(' ');
-//     results1.innerHTML = `Your name is ${splitName[0]}, `;
-//   });
-//   const houseNumber = getRandomNumber(8)
-//   fetch(`https://www.anapioficeandfire.com/api/houses?pagesize=50&page=${houseNumber}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     const number = getRandomNumber(50, 0);
-//     const number2 = getRandomNumber(50, 0);
-//     results2.innerHTML = `you belong in ${data[number].name},`
-//     results3.innerHTML = `and you were born in ${data[number2].region}.`
-//   })
-// }
-
-// const deadOrAlive = () => {
-//   const number = getRandomNumber(100, 0);
-//   if (number % 3 === 0) {
-//     results1.innerHTML = `You're dead 💀`;
-//     results2.innerHTML = `I'm sorry`;
-//     results3.innerHTML = 'The Night is dark and full of terrors.'
-//   } else {
-//     alterEgo();
-//   }
-// }
-
-// btn.addEventListener('click', deadOrAlive);
